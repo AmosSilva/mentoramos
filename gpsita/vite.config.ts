d99b740d8ecb44bdeabbb15a98d2ebf25e294ce8
pedrottoni <pedrottoni@gmail.com>
2025-12-12 14:49:33 -0300

    Updated gpsita/vite.config.ts

--- gpsita/vite.config.ts
+++ gpsita/vite.config.ts
@@ -4,22 +4,22 @@ import react from '@vitejs/plugin-react';
 import dyadComponentTagger from '@dyad-sh/react-vite-component-tagger';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      base: '/gpsita/',
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [dyadComponentTagger(), react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: '/gpsita/', // <-- Importante
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [dyadComponentTagger(), react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
